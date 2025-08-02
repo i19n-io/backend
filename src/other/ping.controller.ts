f93b225d629368709f30f820890558c1a435e327
Vova Revenko <vova@revenko.org>
2025-08-02 21:36:53 +0400

    feat(other): API docs in `PingController`

--- src/other/ping.controller.ts
+++ src/other/ping.controller.ts
@@ -1,9 +1,28 @@
 import { Controller, Get } from '@nestjs/common'
+import {
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
 
+const PONG = 'pong'
+
+class PingResponse {
+  @ApiProperty({ enum: [PONG] })
+  ping = PONG
+}
+
+@ApiTags('Other')
 @Controller('ping')
 export class PingController {
+  @ApiOperation({
+    summary: 'Ping the server',
+    description: 'Returns a simple response to check if the server is running.',
+  })
+  @ApiResponse({ status: 200, type: PingResponse })
   @Get()
   ping() {
-    return { ping: 'pong' }
+    return new PingResponse()
   }
 }
