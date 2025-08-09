778802659cdacf78d35ef6f333ad823eca6115cb
Vova Revenko <vova@revenko.org>
2025-08-10 03:42:21 +0400

    fix(auth): use import of `UserModule` rather than import of `UserService`

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 
 import { SignController } from '~/auth/sign.controller'
 import { SignService } from '~/auth/sign.service'
-import { UserService } from '~/user/user.service'
+import { UserModule } from '~/user/user.module'
 
 @Module({
+  imports: [UserModule],
   controllers: [SignController],
-  providers: [SignService, UserService],
+  providers: [SignService],
 })
 export class AuthModule {}
